ad2a6ca602531e264b7926e8a7bb5b6c232fe66f
Rich Garrick <richg341@gmail.com>
2017-08-25 12:13:37 -0700

    pass test to set id when personal notes created

--- src/main/java/dao/PersonalNotesDao.java
+++ src/main/java/dao/PersonalNotesDao.java
@@ -1,4 +1,21 @@
 package dao;
 
+import models.PersonalNotes;
+
+import java.util.List;
+
 public interface PersonalNotesDao {
+
+    //create
+    void add (PersonalNotes personalNotes);
+
+    //read
+//    PersonalNotes findById(int id);
+//    List<PersonalNotes> getAllPersonalNotesByEntry(int entryId);
+
+    //update
+//    void update(String writtenBy, int entryId, int id, String content);
+
+    //destroy
+//    void deleteById(int id);
 }
--- src/main/java/dao/Sql2oPersonalNotesDao.java
+++ src/main/java/dao/Sql2oPersonalNotesDao.java
@@ -1,6 +1,9 @@
 package dao;
 
+import models.PersonalNotes;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 public class Sql2oPersonalNotesDao implements PersonalNotesDao {
     private final Sql2o sql2o;
@@ -8,4 +11,19 @@ public class Sql2oPersonalNotesDao implements PersonalNotesDao {
     public Sql2oPersonalNotesDao(Sql2o sql2o) {
         this.sql2o = sql2o;
     }
+
+    @Override
+    public void add(PersonalNotes personalNotes){
+        String sql = "INSERT INTO personalNotes (writtenBy, entryId, content) VALUES (:writtenBy, :entryId, :content)";
+        try (Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql)
+                    .bind(personalNotes)
+                    .executeUpdate()
+                    .getKey();
+            personalNotes.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
 }
